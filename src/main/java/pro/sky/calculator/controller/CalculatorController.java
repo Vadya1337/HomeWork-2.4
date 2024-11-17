65cebfb853cee318e3864d3afd34d3e0ed04a7c5
Вадя <vadim.filatov.02@gmail.com>
2024-11-17 15:13:28 +0700

    Исправил дз 4

--- src/main/java/pro/sky/calculator/controller/CalculatorController.java
+++ src/main/java/pro/sky/calculator/controller/CalculatorController.java
@@ -1,4 +1,5 @@
 package pro.sky.calculator.controller;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,38 +9,41 @@ import pro.sky.calculator.service.CalculatorService;
 @RestController
 @RequestMapping(  "/calculator")
 public class CalculatorController {
-    private final CalculatorService calculatorService;
+    private CalculatorService calculatorService;
 
-
-
+    @Autowired
     public CalculatorController(CalculatorService calculatorService) {
         this.calculatorService = calculatorService;
     }
 
-        @GetMapping(path = "/")
-        public String calculator () {
+    @GetMapping(path = "/")
+    public String calculator() {
         return calculatorService.calculator();
     }
 
-        @GetMapping(path = "/plus")
-        public String plus ( @RequestParam(required=false) Integer num1, @RequestParam(required=false) Integer num2){
-        return calculatorService.plus(num1, num2);
-    }
+    @GetMapping(path = "/plus")
+    public String plus(@RequestParam (required = false) Integer num1, @RequestParam (required = false) Integer num2) {
+      calculatorService.validateParams(num1, num2);
+            return calculatorService.plus(num1, num2);
+        }
+
+
 
         @GetMapping(path = "/minus")
         public String minus ( @RequestParam int num1, @RequestParam int num2){
+            calculatorService.validateParams(num1, num2);
         return calculatorService.minus(num1, num2);
-    }
+        }
 
         @GetMapping(path = "/proizvedenie")
         public String proizvedenie ( @RequestParam int num1, @RequestParam int num2){
-        return calculatorService.proizvedenie(num1, num2);
-    }
+            calculatorService.validateParams(num1, num2);
+            return calculatorService.proizvedenie(num1, num2);
+        }
 
         @GetMapping(path = "/divide")
         public String divide ( @RequestParam int num1, @RequestParam int num2){
-        return calculatorService.divide(num1, num2);
+            calculatorService.validateParams(num1, num2);
+            return calculatorService.divide(num1, num2);
+        }
     }
-    }
-
-
--- src/main/java/pro/sky/calculator/service/CalculatorService.java
+++ src/main/java/pro/sky/calculator/service/CalculatorService.java
@@ -9,38 +9,48 @@ public class CalculatorService {
     public String calculator() {
         return "Добро пожаловать в калькулятор!";
     }
-
-    @GetMapping(path = "/plus")
-    public String plus(@RequestParam Integer num1, @RequestParam Integer num2) {
-        if (num1 == null && num2 == null) {
-            return " Не хватает числа для выполнения данного действия!";
-        } else {
-            int result = num1 + num2;
-            return num1 + " + " + num2 + " = " + result;
+    public void validateParams(  Integer num1, Integer num2) {
+        if (num1 == null || num2 == null) {
+            throw new IllegalArgumentException("Не хватает числа для выполнения данного действия!");
         }
     }
 
+    @GetMapping(path = "/plus")
+    public String plus(@RequestParam int num1, @RequestParam int num2) {
+        int result = num1 + num2;
+        return num1 + " + " + num2 + " = " + result;
+
+    }
+
+
     public String minus(int num1, int num2) {
+
         int result = num1 - num2;
         return "Результат = " + num1 + "-" + num2 + "=" + result;
     }
 
     public String proizvedenie(int num1, int num2) {
+
         int result = num1 * num2;
         return "Результат = " + num1 + "*" + num2 + "=" + result;
     }
 
     public String divide(@RequestParam(required = false) Integer num1, @RequestParam(required = false) Integer num2) {
-
         if (num2 == 0) {
             return " Делить на ноль нельзя";
         } else {
             int result = num1 / num2;
             return "Результат = " + num1 + "/" + num2 + "=" + result;
         }
-
     }
+
 }
+
+
+
+
+
+
 
 
 
